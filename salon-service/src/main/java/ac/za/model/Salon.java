0b3649ddc753097091c1c38ccebc4df4c9a80b5c
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-03-07 22:18:00 +0200

    feat(model): add domain model classes

---
+++ salon-service/src/main/java/ac/za/model/Salon.java
@@ -0,0 +1,43 @@
+package ac.za.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+public class Salon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> images;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private Long ownerId;
+
+    @Column(nullable = false)
+    private LocalDateTime openTime;
+
+    @Column(nullable = false)
+    private LocalDateTime closeTime;
+}
