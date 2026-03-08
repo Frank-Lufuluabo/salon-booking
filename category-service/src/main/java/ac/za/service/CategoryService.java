cf9cd96feef8224c6ddfb2df013a5b21ede25672
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-03-08 14:27:57 +0200

    feat: add service layer

---
+++ category-service/src/main/java/ac/za/service/CategoryService.java
@@ -0,0 +1,17 @@
+package ac.za.service;
+
+import ac.za.dto.SalonDTO;
+import ac.za.model.Category;
+
+import java.util.Set;
+
+public interface CategoryService {
+
+    Category saveCategory(Category category, SalonDTO salonDTO);
+
+    Set<Category> getAllCategoriesBySalonId(Long id);
+
+    Category getCategoryById(Long id) throws Exception;
+
+    void deleteCategoryById(Long id, Long salonId) throws Exception;
+}
