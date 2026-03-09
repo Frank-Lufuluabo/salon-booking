5b018fe9a2a2e5dd78a09cc3e545b9f64c37ccd8
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-03-09 23:05:43 +0200

    feat: add service offering repository for service-offering module

---
+++ service-offering/src/main/java/ac/za/repository/ServiceOfferingRepository.java
@@ -0,0 +1,11 @@
+package ac.za.repository;
+
+import ac.za.model.ServiceOffering;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+
+public interface ServiceOfferingRepository extends JpaRepository<ServiceOffering, Long> {
+
+    Set<ServiceOffering> findBySalonId(Long salonId);
+}
