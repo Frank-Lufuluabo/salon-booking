a0bb626fa9dd100f24accd992133b85715c74c42
frankkalengay@gmail.com <frankkalengay@gmail.com>
2026-03-21 21:46:12 +0200

    feat: add repository class for payment-service module

---
+++ payment-service/src/main/java/ac/za/repository/PaymentOrderRepository.java
@@ -0,0 +1,8 @@
+package ac.za.repository;
+
+import ac.za.model.PaymentOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentOrderRepository extends JpaRepository<PaymentOrder, Long> {
+    PaymentOrder findByPaymentLinkId(String paymentLinkId);
+}
